aac00dd3cb42dc3521ea7e47293d926a2f26f09d
janevski.stefan <38108655+janevski400@users.noreply.github.com>
2022-06-02 11:56:22 +0200

    Create 09.06.2021-3-vtora_grupa.c

---
+++ 09.06.2021-3-vtora_grupa.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    FILE *source, *destination;
+    source = fopen("mesto za path", "r");
+    destination = fopen("mesto za path", "w");
+    int intValue_char;
+    int offset = 1;
+    do {
+        fseek(source,-offset,SEEK_END);
+        intValue_char = fgetc(source);
+
+        if(intValue_char == EOF)
+            break;
+
+        offset++;
+        fputc(intValue_char,destination);
+        fflush(destination);
+    } while(1);
+
+    fclose(source);
+    fclose(destination);
+    return 0;
+}
