5e86863cb21c2ef12bd3964aa84a843a73b2b86f
janevski.stefan <38108655+janevski400@users.noreply.github.com>
2022-06-02 11:21:11 +0200

    Initial Commit

---
+++ 09.06.2021-3-prva_grupa.c
@@ -0,0 +1,23 @@
+// Da se napishe programa koja kje go prikazhe brojot na pojavuvanja na cifrata 3 vo tekstualna datoteka so ime treta.txt
+
+#include <stdio.h>
+
+int main() {
+    FILE *file;
+    int countOccurrences = 0;
+    int intValue_char;
+    
+    file = fopen("mesto za path", "r");
+    if(file == NULL) {
+        printf("An error occurred while trying to reading treta.txt");
+        return 1;
+    }
+    while((intValue_char= fgetc(file)) != EOF)
+        if(intValue_char=='3')
+            countOccurrences++;
+
+    fclose(file);
+    printf("The number three appears %d times in treta.txt", countOccurrences);
+
+    return 0;
+}
